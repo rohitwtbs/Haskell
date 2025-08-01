e05894569e4dad7f481e8f0b91ecb5548367c258
rohit kumar <12rohit4@gmail.com>
2025-08-01 14:57:46 +0000

    "split words"

--- basic_string.hs
+++ basic_string.hs
@@ -1,9 +1,31 @@
--- Define a simple function
+-- -- Define a simple function
+-- greet :: String -> String
+-- greet name = "Hello, " ++ name ++ "!"
+
+-- -- split the string alphabets
+
+-- splitString :: String -> [String]
+-- splitString str = words str
+
+-- -- The main function
+-- main :: IO ()
+-- main = do
+--     let name = "Rohit kumar"
+--     putStrLn (greet name)
+--     putStrLn (splitString name)
+
+
+
 greet :: String -> String
 greet name = "Hello, " ++ name ++ "!"
+
+-- Split the string into words
+splitString :: String -> [String]
+splitString str = words str
 
 -- The main function
 main :: IO ()
 main = do
-    let name = "Rohit"
+    let name = "Rohit kumar"
     putStrLn (greet name)
+    print (splitString name)  -- use print instead of putStrLn for list output
